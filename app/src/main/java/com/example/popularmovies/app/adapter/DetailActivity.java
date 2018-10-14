dac5e923c84515505f43124212f262a696047109
Ankur Gupta <guptaankur.gupta0@gmail.com>
2018-10-14 23:46:53 +0530

    Add details activity

---
+++ app/src/main/java/com/example/popularmovies/app/adapter/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.popularmovies.app.adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.popularmovies.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
